807ff9b6336d655a6a16a9c684c91eb264b99b3e
cweijan <cweijan@163.com>
2021-07-04 15:09:49 +0800

    Add vscode web ui ext.

---
+++ static/extensions/vscode-web-ui/main.js
@@ -0,0 +1,75 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else {
+		var a = factory();
+		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
+	}
+})(self, function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 7549:
+/***/ ((module) => {
+
+module.exports = require("vscode");;
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.deactivate = exports.activate = void 0;
+const vscode = __webpack_require__(7549);
+function activate(context) {
+    const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 9999999999);
+    statusBar.command = "workbench.action.showCommands";
+    statusBar.text = `$(debug-console-evaluation-prompt)`; // https://code.visualstudio.com/api/references/icons-in-labels
+    statusBar.backgroundColor = new vscode.ThemeColor("statusBarItem.remoteBackground");
+    statusBar.show();
+    statusBar.tooltip = "Command Palette";
+    context.subscriptions.push(statusBar);
+}
+exports.activate = activate;
+function deactivate() { }
+exports.deactivate = deactivate;
+
+})();
+
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
